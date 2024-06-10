b27612bce1bf85ae4dea92fdbf30cd8a8db64752
Ahmed Okasha <75159786+A-Oka-jr@users.noreply.github.com>
2024-06-10 12:22:03 +0400

    testing conniction

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,12 +1,16 @@
-import mongoose from 'mongoose';
-import 'dotenv/config';
+import mongoose from "mongoose";
+import "dotenv/config";
 
 const dbConnect = async () => {
   try {
-    await mongoose.connect(`mongodb+srv://iscoa020:${process.env.DB_PASSWORD}@reporting.qicbunc.mongodb.net/?retryWrites=true&w=majority&appName=${process.env.DB_NAME}`);
-    console.log('Connected to MongoDB!');
+    await mongoose.connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      serverSelectionTimeoutMS: 5000, // Adjust this timeout as needed
+    });
+    console.log("Connected to MongoDB!");
   } catch (error) {
-    console.error('Error connecting to MongoDB:', error);
+    console.error("Error connecting to MongoDB:", error);
   }
 };
 
