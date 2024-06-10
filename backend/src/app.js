cfad506df11cacef35bbfa9641c4062d4b44a9fe
Ahmed Okasha <75159786+A-Oka-jr@users.noreply.github.com>
2024-06-10 11:54:51 +0400

    fixing errors

--- backend/src/app.js
+++ backend/src/app.js
@@ -22,7 +22,7 @@ app.use("/api/user", userRouter);
 app.use("/api/client", clientRouter);
 app.use("/api/complaint", complaintRouter);
 
-app.use(express.static(path.join(__dirname, "../client/dist")));
+app.use(express.static(path.join(__dirname, "/client/dist")));
 
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
